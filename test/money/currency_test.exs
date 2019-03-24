5cf72741a75007bf22e77d097abacb99a933d2d3
William Takeshi Pereira <william.takeshi@sensedata.com.br>
2019-03-24 18:44:44 -0300

    start currency and add currency_test

---
+++ test/money/currency_test.exs
@@ -0,0 +1,11 @@
+defmodule Money.CurrencyTest do
+  use ExUnit.Case, async: true
+  doctest Money.Currency
+
+  alias Money.Currency
+
+  test "exists?/1" do
+    assert Currency.exists?(:USD)
+    refute Currency.exists?(:ZZZ)
+  end
+end
